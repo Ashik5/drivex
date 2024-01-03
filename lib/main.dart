9c4388c360f1a4056c36bddba748869f9dba2b6a
tanjir238 <tanjir238@gmail.com>
2024-01-04 04:05:00 +0600

    Driver Documents Upload 01

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,17 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'login_page.dart';
 import 'sign_up_page.dart';
 import 'profile_management.dart';
+import 'firebase_options.dart';
+import 'driver_document.dart';
+
+final List<String> brands = <String>['bmw', 'audi', 'toyota', 'mercedes'];
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
   await Firebase.initializeApp(
-      options: const FirebaseOptions(
-          apiKey: "AIzaSyCX9W_kYbdvulFvYbS1xU_SO4Lt083ryk8",
-          appId: "1:478669367974:android:f711fcb3a1b169baa46267",
-          messagingSenderId: "478669367974",
-          projectId: "latest01-2b8fa"));
-  await Firebase.initializeApp();
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -25,10 +25,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Homepage(),
-
       title: 'Testing flutter',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -40,9 +38,10 @@ class MyApp extends StatelessWidget {
         '/home': (context) => const HomeScreen(),
         '/login': (context) => const LoginPage(),
         '/signup': (context) => const SignUp(),
-        '/profile' : (context) => ProfileManagement(),
+        '/profile' : (context) => const ProfileManagement(),
         '/signupDetails': (context) => const Details(),
-        '/profile': (context) => ProfileManagement(),
+        '/driverDocument' : (context) => const Driver(),
+        '/driverDetails' : (context) => const Driver_details(),
       },
     );
   }
@@ -132,19 +131,11 @@ class SplashScreen extends StatelessWidget {
   }
 }
 
-class Homepage extends StatelessWidget {
-  const Homepage({super.key});
-
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: ElevatedButton(
-          onPressed: () {
-            FirebaseAuth.instance.createUserWithEmailAndPassword(
-                email: 'tanjir@gmail.com', password: '12345678');
-          },
-          child: const Text('press'),
       body: ListView(
         children: [
           Container(
@@ -550,6 +541,45 @@ class CustomNavigationBar extends StatelessWidget {
           top: Radius.circular(20),
           bottom: Radius.circular(0),
         ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          IconButton(
+              onPressed: () {},
+              icon: SvgPicture.asset(
+                'assets/icons/nav/home.svg',
+                height: 30,
+              )),
+          IconButton(
+              onPressed: () {},
+              icon: SvgPicture.asset(
+                'assets/icons/nav/location.svg',
+                height: 30,
+              )),
+          IconButton(
+              onPressed: () {},
+              icon: SvgPicture.asset(
+                'assets/icons/nav/liked.svg',
+                height: 30,
+              )),
+          IconButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/driverDocument');
+              },
+              icon: SvgPicture.asset(
+                'assets/icons/nav/chat.svg',
+                height: 30,
+              )),
+          IconButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/profile');
+              },
+              icon: SvgPicture.asset(
+                'assets/icons/nav/user.svg',
+                height: 30,
+              ))
+        ],
       ),
     );
   }
