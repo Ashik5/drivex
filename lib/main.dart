0ac1a8078f7c823a51a0ed485da06d0eb993c7d0
Ashik Mahmud <ashikmamud123@gmail.com>
2024-01-03 23:57:32 +0600

    perfectly working

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -6,9 +5,11 @@ import 'login_page.dart';
 import 'sign_up_page.dart';
 import 'profile_management.dart';
 
+final List<String> brands = <String>['bmw', 'audi', 'toyota', 'mercedes'];
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   await Firebase.initializeApp(
       options: const FirebaseOptions(
           apiKey: "AIzaSyCX9W_kYbdvulFvYbS1xU_SO4Lt083ryk8",
@@ -25,10 +26,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Homepage(),
-
       title: 'Testing flutter',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -40,9 +39,8 @@ class MyApp extends StatelessWidget {
         '/home': (context) => const HomeScreen(),
         '/login': (context) => const LoginPage(),
         '/signup': (context) => const SignUp(),
-        '/profile' : (context) => ProfileManagement(),
+        '/profile': (context) => const ProfileManagement(),
         '/signupDetails': (context) => const Details(),
-        '/profile': (context) => ProfileManagement(),
       },
     );
   }
@@ -55,96 +53,89 @@ class SplashScreen extends StatelessWidget {
     return SafeArea(
         child: Scaffold(
             body: Center(
-              child: Container(
-                padding: const EdgeInsets.symmetric(vertical: 20),
-                width: double.infinity,
-                child: Column(children: [
-                  Expanded(
-                      child: Image.asset(
-                        'assets/img/front-img.png',
-                        fit: BoxFit.fill,
-                      )),
-                  const SizedBox(height: 20),
-                  const SizedBox(
-                    width: 330,
-                    child: Text.rich(
-                      TextSpan(text: 'Ultimate ', children: <TextSpan>[
-                        TextSpan(
-                            text: 'car rental ',
-                            style: TextStyle(color: Color.fromRGBO(50, 132, 255, 1))),
-                        TextSpan(
-                          text: ' experience',
-                        )
-                      ]),
-                      softWrap: true,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        height: 0.8,
-                        fontWeight: FontWeight.normal,
-                        fontSize: 32,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 15),
-                  const SizedBox(
-                    width: 350,
-                    child: Text(
-                      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout',
-                      softWrap: true,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        height: 1.2,
-                        fontSize: 14,
-                        color: Color.fromRGBO(0, 0, 0, 0.6),
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/signup');
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromRGBO(12, 37, 81, 1),
-                      shape: RoundedRectangleBorder(
-                        side: BorderSide.none,
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                    ),
-                    child: const Text(
-                      'Let’s get started',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/login');
-                    },
-                    child: const Text(
-                      'Log in',
-                      style: TextStyle(color: Colors.blue),
-                    ),
-                  )
-                ]),
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 20),
+        width: double.infinity,
+        child: Column(children: [
+          Expanded(
+              child: Image.asset(
+            'assets/img/front-img.png',
+            fit: BoxFit.fill,
+          )),
+          const SizedBox(height: 20),
+          const SizedBox(
+            width: 330,
+            child: Text.rich(
+              TextSpan(text: 'Ultimate ', children: <TextSpan>[
+                TextSpan(
+                    text: 'car rental ',
+                    style: TextStyle(color: Color.fromRGBO(50, 132, 255, 1))),
+                TextSpan(
+                  text: ' experience',
+                )
+              ]),
+              softWrap: true,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                height: 0.8,
+                fontWeight: FontWeight.normal,
+                fontSize: 32,
               ),
-            )));
+            ),
+          ),
+          const SizedBox(height: 15),
+          const SizedBox(
+            width: 350,
+            child: Text(
+              'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout',
+              softWrap: true,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                height: 1.2,
+                fontSize: 14,
+                color: Color.fromRGBO(0, 0, 0, 0.6),
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/signup');
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromRGBO(12, 37, 81, 1),
+              shape: RoundedRectangleBorder(
+                side: BorderSide.none,
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+            child: const Text(
+              'Let’s get started',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/login');
+            },
+            child: const Text(
+              'Log in',
+              style: TextStyle(color: Colors.blue),
+            ),
+          )
+        ]),
+      ),
+    )));
   }
 }
 
-class Homepage extends StatelessWidget {
-  const Homepage({super.key});
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: ElevatedButton(
-          onPressed: () {
-            FirebaseAuth.instance.createUserWithEmailAndPassword(
-                email: 'tanjir@gmail.com', password: '12345678');
-          },
-          child: const Text('press'),
       body: ListView(
         children: [
           Container(
@@ -350,7 +341,7 @@ class Homepage extends StatelessWidget {
           ),
         ],
       ),
-      bottomNavigationBar: CustomNavigationBar(),
+      bottomNavigationBar: const CustomNavigationBar(),
     );
   }
 }
@@ -450,7 +441,7 @@ class CarCard extends StatelessWidget {
                   ),
                   child: Padding(
                     padding:
-                    const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+                        const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
                     child: Text(
                       brandName,
                       style: const TextStyle(
