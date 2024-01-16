63905d274c4519b18b2b61bcaca362b9ac73cc2a
Elman7 <elmannayeem1@gmail.com>
2024-01-16 19:48:35 +0600

    profile-management new

--- lib/profile_management.dart
+++ lib/profile_management.dart
@@ -1,25 +1,146 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
+
+void main() {
+  runApp(const ProfileManagement());
+}
 
 class ProfileManagement extends StatelessWidget {
-  ProfileManagement({Key? key}) : super(key: key);
-  final user = FirebaseAuth.instance.currentUser;
+  const ProfileManagement({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: const Color.fromARGB(255, 18, 32, 47),
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile Management'),
       ),
-      home: Scaffold(
-        body: ListView(children: [
-          ElevatedButton(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          CustomButton("EDIT PROFILE", () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const EditProfilePage()),
+            );
+          }),
+          CustomButton("SETTINGS", () {
+            // Add settings functionality here
+          }),
+          CustomButton("BILLING DETAILS", () {
+            // Add billing details functionality here
+          }),
+          CustomButton("INFORMATION", () {
+            // Add information functionality here
+          }),
+          CustomButton("LOG OUT", () {
+            // Add log out functionality here
+          }),
+        ],
+      ),
+    );
+  }
+}
+
+class EditProfilePage extends StatefulWidget {
+  const EditProfilePage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _EditProfilePageState createState() => _EditProfilePageState();
+}
+
+class _EditProfilePageState extends State<EditProfilePage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController mobileController = TextEditingController();
+  TextEditingController birthdayController = TextEditingController();
+  TextEditingController addressController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Edit Profile'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: const InputDecoration(labelText: 'Name'),
+            ),
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              controller: mobileController,
+              decoration: const InputDecoration(labelText: 'Mobile Number'),
+            ),
+            TextField(
+              controller: birthdayController,
+              decoration: const InputDecoration(labelText: 'Birthday'),
+            ),
+            TextField(
+              controller: addressController,
+              decoration: const InputDecoration(labelText: 'Address'),
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
               onPressed: () {
-                FirebaseAuth.instance.signOut();
-                Navigator.pushNamed(context, '/');
+                // Add logic to save user data here
+                Navigator.pop(context); // Go back to the home page
               },
-              child: Text("sign out")),
-        ]),
+              child: const Text('Save'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final String title;
+  final VoidCallback onPressed;
+
+  const CustomButton(this.title, this.onPressed, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        child: Text(title),
       ),
     );
   }
