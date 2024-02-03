0f0e52d0a58f741a9bfd2882e336f9778379cf11
Elman7 <elmannayeem1@gmail.com>
2024-02-04 01:17:04 +0600

    fetch image

--- lib/profile_management.dart
+++ lib/profile_management.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
 
 class ProfileManagement extends StatelessWidget {
   const ProfileManagement({super.key});
@@ -53,8 +58,10 @@ class HomePage extends StatelessWidget {
   }
 }
 
+
+
 class EditProfilePage extends StatefulWidget {
-  const EditProfilePage({super.key});
+  const EditProfilePage({Key? key}) : super(key: key);
 
   @override
   _EditProfilePageState createState() => _EditProfilePageState();
@@ -66,6 +73,7 @@ class _EditProfilePageState extends State<EditProfilePage> {
   TextEditingController mobileController = TextEditingController();
   TextEditingController birthdayController = TextEditingController();
   TextEditingController addressController = TextEditingController();
+  File? _image;
 
   // Reference to the Firebase collection
   final CollectionReference usersCollection =
@@ -76,6 +84,7 @@ class _EditProfilePageState extends State<EditProfilePage> {
     super.initState();
     // Fetch and display user data when the page loads
     fetchAndDisplayUserData();
+
   }
 
   Future<void> fetchAndDisplayUserData() async {
@@ -83,81 +92,32 @@ class _EditProfilePageState extends State<EditProfilePage> {
     final User? user = FirebaseAuth.instance.currentUser;
 
     if (user != null) {
-      // Fetch user data from Firebase
-      DocumentSnapshot userSnapshot = await usersCollection.doc(user.uid).get();
+      try {
+        // Fetch user data from Firebase
+        DocumentSnapshot userSnapshot = await usersCollection.doc(user.uid).get();
 
-      // Set the retrieved data in the text controllers
-      nameController.text = userSnapshot['name'] ?? '';
-      emailController.text = userSnapshot['email'] ?? '';
-      mobileController.text = userSnapshot['mobile'] ?? '';
-      birthdayController.text = userSnapshot['birthday'] ?? '';
-      addressController.text = userSnapshot['address'] ?? '';
+        // Set the retrieved data in the text controllers
+        nameController.text = userSnapshot['name'] ?? '';
+        emailController.text = userSnapshot['email'] ?? '';
+        mobileController.text = userSnapshot['mobile'] ?? '';
+        birthdayController.text = userSnapshot['birthday'] ?? '';
+        addressController.text = userSnapshot['address'] ?? '';
+
+        // Retrieve and set the profile image
+        dynamic profileImage = userSnapshot['profileImage'];
+        if (profileImage is String) {
+          setState(() {
+            _image = File(profileImage);
+          });
+        }
+      } catch (e) {
+        // Handle any errors that might occur during data fetching
+        print('Error fetching user data: $e');
+      }
     }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Edit Profile'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            TextField(
-              controller: nameController,
-              decoration: const InputDecoration(labelText: 'Name'),
-            ),
-            TextField(
-              controller: emailController,
-              decoration: const InputDecoration(labelText: 'Email'),
-            ),
-            TextField(
-              controller: mobileController,
-              decoration: const InputDecoration(labelText: 'Mobile Number'),
-            ),
-            TextField(
-              controller: birthdayController,
-              decoration: InputDecoration(
-                labelText: 'Birthday',
-                suffixIcon: IconButton(
-                  icon: const Icon(Icons.calendar_today),
-                  onPressed: () async {
-                    DateTime? selectedDate = await showDatePicker(
-                      context: context,
-                      initialDate: DateTime.now(),
-                      firstDate: DateTime(1900),
-                      lastDate: DateTime.now(),
-                    );
 
-                    if (selectedDate != null) {
-                      birthdayController.text =
-                      "${selectedDate.day}/${selectedDate.month}/${selectedDate.year}";
-                    }
-                  },
-                ),
-              ),
-            ),
-            TextField(
-              controller: addressController,
-              decoration: const InputDecoration(labelText: 'Address'),
-            ),
-            const SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () async {
-                // Add logic to save user data to Firebase
-                await saveUserData();
-                Navigator.pop(context); // Go back to the home page
-              },
-              child: const Text('Save'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
 
   Future<void> saveUserData() async {
     // Get the current user
@@ -174,7 +134,114 @@ class _EditProfilePageState extends State<EditProfilePage> {
       });
     }
   }
+
+  Future<void> _uploadImage() async {
+    final User? user = FirebaseAuth.instance.currentUser;
+
+    if (user != null && _image != null) {
+      // Create a unique filename for the image based on the user's UID
+      String fileName = 'profile_${user.uid}.jpg';
+
+      // Upload the image to Firebase Storage
+      Reference storageReference =
+      FirebaseStorage.instance.ref().child('profile_images/$fileName');
+      await storageReference.putFile(_image!);
+
+      // Get the download URL of the uploaded image
+      String downloadURL = await storageReference.getDownloadURL();
+
+      // Save the download URL to the user's profile in Firestore
+      await usersCollection.doc(user.uid).update({'profileImage': downloadURL});
+    }
+  }
+
+  Future<void> _getImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Edit Profile'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // Image display
+              _image == null
+                  ? const SizedBox.shrink()
+                  : Image.file(_image!),
+              // Upload Image button
+              ElevatedButton(
+                onPressed: _getImage,
+                child: const Text('Upload Photo'),
+              ),
+              TextField(
+                controller: nameController,
+                decoration: const InputDecoration(labelText: 'Name'),
+              ),
+              TextField(
+                controller: emailController,
+                decoration: const InputDecoration(labelText: 'Email'),
+              ),
+              TextField(
+                controller: mobileController,
+                decoration: const InputDecoration(labelText: 'Mobile Number'),
+              ),
+              TextField(
+                controller: birthdayController,
+                decoration: InputDecoration(
+                  labelText: 'Birthday',
+                  suffixIcon: IconButton(
+                    icon: const Icon(Icons.calendar_today),
+                    onPressed: () async {
+                      DateTime? selectedDate = await showDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate: DateTime(1900),
+                        lastDate: DateTime.now(),
+                      );
+
+                      if (selectedDate != null) {
+                        birthdayController.text =
+                        "${selectedDate.day}/${selectedDate.month}/${selectedDate.year}";
+                      }
+                    },
+                  ),
+                ),
+              ),
+              TextField(
+                controller: addressController,
+                decoration: const InputDecoration(labelText: 'Address'),
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () async {
+                  await _uploadImage();
+                  await saveUserData();
+                  Navigator.pop(context);
+                },
+                child: const Text('Save'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
+
 
 class CustomButton extends StatelessWidget {
   final String title;
